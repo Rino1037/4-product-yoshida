c64711fe0e891ad41bf870ea428f74753a5aff81
jzhu2022 <jjuliazhu4@gmail.com>
2019-12-19 23:38:10 -0500

    Final touches

---
+++ js/plus-minus.js
@@ -0,0 +1,23 @@
+function switchPlusMinus(x) {
+    document.getElementById("icon-1").classList.toggle("fa-minus");
+}
+
+function switchPlusMinus2(x) {
+    document.getElementById("icon-2").classList.toggle("fa-minus");
+}
+
+function switchPlusMinus3(x) {
+    document.getElementById("icon-3").classList.toggle("fa-minus");
+}
+
+function switchPlusMinus4(x) {
+    document.getElementById("icon-4").classList.toggle("fa-minus");
+}
+
+function switchPlusMinus5(x) {
+    document.getElementById("icon-5").classList.toggle("fa-minus");
+}
+
+function switchPlusMinus6(x) {
+    document.getElementById("icon-6").classList.toggle("fa-minus");
+}
